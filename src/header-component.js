11e65d2f875ac3072029ddfa4c4434088f365897
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-03-14 11:00:29 -0700

    made header component test and passed for user and header

---
+++ src/header-component.js
@@ -0,0 +1,27 @@
+export function makeHeader(){
+    const html = /*html*/ `
+        <header>
+            <img src="./assets/alchemy-logo.png" alt="alchemy logo">
+            <h1 class="header-logo">Favorite News</h1>
+        </header>
+        `;
+
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+
+}
+
+export function makeProfile(user) {
+    const html = /*html*/ ` 
+    <div class="profile">
+        <img src="${user.photoURL}" alt="avatar image">
+        <span>${user.displayName}</span>
+        <button>Sign Out</button>
+    </div>
+    `;
+
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
---
+++ test/header-component.test.js
@@ -0,0 +1,43 @@
+import { makeHeader, makeProfile } from '../src/header-component.js';
+const test = QUnit.test;
+
+QUnit.module('make template literal function for header');
+
+
+test('template literal for header', assert => {
+    //arrange
+
+    //act
+    const expected = /*html*/ `
+        <header>
+            <img src="./assets/alchemy-logo.png" alt="alchemy logo">
+            <h1 class="header-logo">Favorite News</h1>
+        </header>
+    `;
+
+    const result = makeHeader();
+    //assert
+    assert.htmlEqual(result, expected);
+
+});
+
+test('temp literal for profile display', assert => {
+    //arrange
+    const user = {
+        displayName: 'Colin Coutts',
+        photoURL: 'https://iupac.org/wp-content/uploads/2018/05/default-avatar.png'
+    };
+
+    //act
+    const expected = /*html*/ ` 
+    <div class="profile">
+        <img src="https://iupac.org/wp-content/uploads/2018/05/default-avatar.png" alt="avatar image">
+        <span>Colin Coutts</span>
+        <button>Sign Out</button>
+    </div>
+    `;
+
+    const result = makeProfile(user);
+    //assert
+    assert.htmlEqual(result, expected);
+});
